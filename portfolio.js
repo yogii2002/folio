ff4090aea3dc7af665d09869f73a0503c6686a5d
Yogesh Jat <81260380+yogii2002@users.noreply.github.com>
2024-06-25 01:54:46 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+<div class="project-container">
+                <div class="project-card" id="project1">
+                    <div class="project-number project-number-right">01</div>
+                    <div class="project-content project-content-left">
+                        <div class="project-skills-container">
+                            <img src="HTML.png" alt="" class="project-skill">
+                            <img src="CSS.png" alt="" class="project-skill">
+                            <img src="Javascript.svg" alt="" class="project-skill">
+                        </div>
+                        <h2 class="project-heading">Weather App</h2>
+                        <div class="project-subHeading">It's a simple weather app that fetch data from the  weather API</div>
+                        <div class="btn-group">
+                            <button class="btn-pink btn-project">Click here</button>
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
